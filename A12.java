0de4e437058a0250151be6573a4e8b5ee3929916
Raj Kiran <rjkiran1619@gmail.com>
2025-04-12 13:19:35 +0530

    sample

---
+++ A12.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("enter the string :");
+     Scanner a = new Scanner(System.in);
+     String b = a.nextLine();
+     System.out.println(b);
+     
+	}
+
+}
